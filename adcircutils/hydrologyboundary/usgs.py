e7206d3619026af64eb70b8f223e743154df264e
Shintaro Bunya <sbunya@gmail.com>
2024-10-23 12:42:26 -0400

    Adding retries in fetching usgs data

--- adcircutils/hydrologyboundary/usgs.py
+++ adcircutils/hydrologyboundary/usgs.py
@@ -1,7 +1,12 @@
-def getQH(siteids,staDT,endDT):
+def getQH(siteids,staDT,endDT,cachedir=None):
     import requests
     import urllib.parse
     from datetime import datetime
+    import time
+    import hashlib
+    import os
+    from pathlib import Path
+    import json
 
     if type(siteids) != list:
         siteids = [siteids]
@@ -18,9 +23,41 @@ def getQH(siteids,staDT,endDT):
         params = {'format': 'json', 'sites': siteid, 'startDT': stStaDT, 'endDT': stEndDT, 'parameterCd': '00060,00065', 'siteType': 'ST', 'siteStatus':'all'}
         params_encoded = urllib.parse.urlencode(params)
         api_url = 'https://nwis.waterservices.usgs.gov/nwis/iv/?'+params_encoded
-        print(api_url)
-        response = requests.get(api_url)
-        json_res = response.json()
+        if cachedir:
+            hasher = hashlib.md5()
+            hasher.update(api_url.encode('utf-8'))
+            cachefile = os.path.join(cachedir, hasher.hexdigest()+'.json')
+        else:
+            cachefile = None
+        print('Target = {}'.format(api_url))
+        if cachefile and Path(cachefile).is_file():
+            print('Found in local cache: {:s}. Loading.'.format(cachefile))
+            with open(cachefile, 'r') as f:
+                json_res = json.load(f)
+        else:
+            print('Not found in local cache. Downloading.')
+            cnt = 0
+            while True:
+                try:
+                    response = requests.get(api_url)
+                    json_res = response.json()
+                    break
+                except requests.JSONDecodeError as e:
+                    cnt += 1
+                    print('JSONDecodeError: {}'.format(e))
+                    print('Response content: {}'.format(response.content))
+                    if cnt < 10:
+                        print('Retrying in 2 seconds...')
+                    else:
+                        print('Failed to decode JSON response after 10 attempts. Raising exception.')
+                        raise e
+                time.sleep(2)
+            
+
+
+            if cachefile:
+                with open(cachefile, 'w') as f:
+                    json.dump(json_res, f)
 
         # Discharge
         nQ = len(json_res['value']['timeSeries'][0]['values'][0]['value'])
