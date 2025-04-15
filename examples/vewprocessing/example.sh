1cf22ad661b84344cd25eacde9d9526112e7964f
Shintaro Bunya <sbunya@gmail.com>
2025-04-15 14:44:48 -0400

    Updating vewprocessing example

---
+++ examples/vewprocessing/example.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+echo "===================================================================="
+echo "Processing VEWs in ADCIRC mesh"
+echo "===================================================================="
+
+echo ""
+echo "1. Converting VEW polylines to node strings of a mesh in YAML format"
+echo "-----------------------------------------------------------------------"
+python -m adcircutils.vewprocessing.polyline_converter input/mesh_without_vews.14 input/vew_polylines.shp -o output/vewstrings.yaml -n 0.03 -e 2.0
+
+echo ""
+echo "2. Adding VEWs to the mesh"
+echo "-----------------------------------------------------------------------"
+python -m adcircutils.vewprocessing.vew_adder input/mesh_without_vews.14 output/vewstrings.yaml -o output/mesh_with_vews.14
+
+echo ""
+echo "3. Scraping VEWs from the mesh"
+echo "-----------------------------------------------------------------------"
+python -m adcircutils.vewprocessing.vew_scraper output/mesh_with_vews.14 -o output/mesh_vews_scraped.14 -y output/vewstrings_scraped.yaml
+
+echo ""
+echo "All done!"
